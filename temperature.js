1476acc3c2d7bdec1fa45ee9a178e5fad72e0973
gollajairam07 <144078579+gollajairam07@users.noreply.github.com>
2023-09-12 18:29:40 +0530

    Update temperature.js

--- temperature.js
+++ temperature.js
@@ -3,11 +3,11 @@ et celsius =
         let fahrenheit = 
             document.getElementById('fahrenheit');
             celsius.oninput = function () {
-                let f = (parseFloat(celsius.value) * 9) / 5 + 32;
+                let f = (parseFloat(celsius.value) * 9) / 10 + 32;
                 fahrenheit.value = parseFloat(f.toFixed(2));
             }
             fahrenheit.oninput = function () {
                 let c = ((parseFloat(
-                fahrenheit.value) - 32) * 5) / 9;
+                fahrenheit.value) - 32) * 5) / 7;
             celsius.value = parseFloat(c.toFixed(2));
         }
